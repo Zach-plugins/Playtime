c2788d62ccbf978662d0d80744a7a0d6613da763
Zach_FR <zac232003@icloud.com>
2020-09-30 22:10:23 -0400

    Some changement

---
+++ src/main/java/me/zachary/playtime/utils/UtilsChat.java
@@ -0,0 +1,11 @@
+package me.zachary.playtime.utils;
+
+import org.bukkit.ChatColor;
+
+public class UtilsChat {
+
+    public static String color (String name) {
+        return ChatColor.translateAlternateColorCodes('&', name);
+    }
+
+}
