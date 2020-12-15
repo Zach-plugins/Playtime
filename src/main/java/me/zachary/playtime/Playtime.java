4817277749fef4fab9792a0998a9015721438781
Zach_FR <zac232003@icloud.com>
2020-12-15 15:28:04 -0500

    Create 4 placeholders

--- src/main/java/me/zachary/playtime/Playtime.java
+++ src/main/java/me/zachary/playtime/Playtime.java
@@ -3,6 +3,7 @@ package me.zachary.playtime;
 import me.zachary.playtime.commands.CommandPlaytime;
 import me.zachary.zachcore.ZachCorePlugin;
 import me.zachary.zachcore.utils.Metrics;
+import org.bukkit.Bukkit;
 
 public final class Playtime extends ZachCorePlugin {
 
@@ -11,6 +12,8 @@ public final class Playtime extends ZachCorePlugin {
         saveDefaultConfig();
 
         new CommandPlaytime(this);
+        if(Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null)
+            new Placeholder(this).register();
 
         int pluginId = 9153;
         Metrics metrics = new Metrics(this, pluginId);
